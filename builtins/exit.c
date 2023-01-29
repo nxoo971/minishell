f8f4025c8e17a6608948a9e6702ca80d41c1d4ba
Jean-marc WANCTIN <jewancti@e2r4p21.clusters.42paris.fr>
2023-01-29 02:05:27 +0100

    fix

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jewancti <jewancti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 21:14:28 by jewancti          #+#    #+#             */
-/*   Updated: 2023/01/28 16:52:44 by jewancti         ###   ########.fr       */
+/*   Updated: 2023/01/29 02:04:55 by jewancti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,75 +15,47 @@
 #define LLMAX	9223372036854775807LL
 #define LLMIN	-9223372036854775807LL
 
-int	no_args(char **argument)
+static void exit_msg(const char *tmp, t_data *data)
 {
-	if (!argument || !*argument)
-	{
-		ft_putendl("exit");
-		return (EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
-}
-
-static
-void	end_exit(t_data *data, char **argument)
-{
-	int	ex;
-
-	ft_putendl("exit");
-	ex = 0;
-	if (argument[1] != NULL)
-		ex = ft_atoull(argument[1]) % 256;
+	ft_printf("bash: exit: %s: numeric argument required\n", tmp);
 	free_shell(data);
-	exit(ex);
-}
-
-int	sub_exit(t_data *data, char *argument, char *b_argument, int *index[2])
-{
-	long long	res;
-
-	res = ft_atoull(argument);
-	if (*argument == '+' || *argument == '-')
-		argument++;
-	while (ft_isdigit(*argument))
-	{
-		(*index)[1]++;
-		argument++;
-	}
-	if ((*index)[0] > 1)
-	{
-		ft_printf("exit\nbash: exit: too many arguments\n", argument);
-		return (update_status_code(data, 127), EXIT_FAILURE);
-	}
-	if (*argument != '\0' || res > LLMAX || res < LLMIN)
-	{
-		if (!*argument)
-			argument = b_argument;
-		ft_printf("bash: exit: %s: numeric argument required\n", argument);
-		free_shell(data);
-		exit(2);
-	}
-	return (EXIT_SUCCESS);
+	exit(2);
 }
 
 void	is_exit(t_data *data, char **argument)
 {
+	const char	*tmp;
+	long long	res;
 	int			index;
-	int			index2;
-	int			isdigit;
-	int			retur;
+	int			a;
 
-	if (no_args(argument))
+	if (!argument || !*argument)
+	{
+		ft_putendl("exit");
 		return ;
+	}
 	index = 0;
-	isdigit = 0;
 	while (argument[++index])
 	{
-		index2 = index;
-		retur = sub_exit(data, argument[index], argument[index], \
-							(int *[2]){& index2, & isdigit});
-		if (retur)
+		tmp = argument[index];
+		res = ft_atoull(tmp);
+		if (*tmp == '+' || *tmp == '-')
+			tmp ++;
+		while (ft_isdigit(*tmp))
+			tmp++;
+		if (index > 1)
+		{
+			ft_printf("exit\nbash: exit: too many arguments\n");
+			update_status_code(data, 127);
 			return ;
+		}
+		if (*tmp != '\0' || res > LLMAX || res < LLMIN)
+			exit_msg(tmp, data);
 	}
-	end_exit(data, argument);
+	ft_putendl("exit");
+	a = EXIT_SUCCESS;
+	if (argument[1] != NULL)
+		a = ft_atoull(argument[1]) % 256;
+	free_shell(data);
+	exit(a);
 }
